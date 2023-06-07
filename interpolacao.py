759ebf963f5436b76bd9a7b29b1bd84ec06e421a
Carlos Henrique <chcruz64@gmail.com>
2023-06-06 21:10:23 -0300

    up

---
+++ interpolacao.py
@@ -0,0 +1,17 @@
+import math
+
+x = [-1, 0, 2]
+fx = [4, 1, -1]
+a = [1, 1, 1]
+p = []
+
+def f(x):
+    return  x
+
+j = -1 
+def p(i):
+    aux = a[0]*x[i] +a[1]*x[i] + a[2]*x[i]**2
+    return p[i]
+
+aux = p(0)
+print(aux)
